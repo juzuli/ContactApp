89719d70da7578d83d9055400fc349f847624f76
almubdie <bibychackokpply@gmail.com>
2022-03-19 13:55:59 +0530

    project structure initialised

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:contact_app/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/src/bloc/contact_state.dart
@@ -0,0 +1,31 @@
+part of 'contact_cubit.dart';
+
+abstract class ContactState extends Equatable {
+  const ContactState();
+}
+
+class ContactInitial extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+
+class Contactloading extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+
+class ContactLoaded extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+class ContactLoadError extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+
+
+
