79ab394a45e2c22fafc2f32f97302c17416ac20b
Juzuli <juzulitry@gmail.com>
2022-03-23 15:33:06 +0530

    Login & Signup page implemented

---
+++ lib/src/constants/strings.dart
@@ -0,0 +1,19 @@
+
+class Strings{
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String forgotPassword = "Forgot your password";
+  static const String signIn = "Sign In";
+  static const String dontHaveAccount = "Don't have an account";
+  static const String create = "Create";
+  static const String name = "Name";
+  static const String phone = "Phone";
+  static const String conformPassword = "Conform password";
+  static const String register = "Register";
+  static const String enterYourEmail = "Enter your email";
+  static const String enterYourPassword = "Enter your password";
+  static const String enterYourConformPassword = "Enter your conform password";
+  static const String enterYourName = "Enter your name";
+  static const String enterYourPhone = "Enter your phone number";
+}
+
